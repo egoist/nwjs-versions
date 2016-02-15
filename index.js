0b4e55d4626e466f65d797f3afb5a5063b0da5cf
EGOIST <0x142857@gmail.com>
2016-02-15 11:41:03 +0800

    remove all const

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var isSemver = require('is-semver')
 module.exports = function () {
 	return aimer('http://dl.nwjs.io/')
 		.then(function ($) {
-			const versions = []
+			var versions = []
 			$('tr').each(function () {
-				let v = $(this).find('a').text()
+				var v = $(this).find('a').text()
 				v = v.substring(1, v.length - 1)
 				if (isSemver(v)) {
 					versions.push(v)
